16569cd92c0e913625278aa9504aeedca977384b
XverLxrd <tur-2002@mail.ru>
2025-06-15 11:34:39 +0700

    Создание схемы БД

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaClient } from './generated/prisma';
 
 dotenv.config()
 
